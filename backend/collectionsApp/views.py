fbac4a1b4d32d81902945b950af2861fb5442258
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2025-07-26 18:46:08 +0530

    fix: remove unused image files and implement put method for collection updates

--- backend/collectionsApp/views.py
+++ backend/collectionsApp/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Collection
 from .serializers import CollectionSerializer
+from django.shortcuts import get_object_or_404
 
 class CollectionList(APIView):
     def get(self, request):
@@ -16,10 +17,18 @@ class CollectionList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
 
 class CollectionById(APIView):
     def get(self, request, id):
         collection = Collection.objects.get(id = id)
         serializer = CollectionSerializer(collection)
         return Response(serializer.data)
+    
+    def put(self, request, id):
+        collection = get_object_or_404(Collection, id=id)
+        serializer = CollectionSerializer(collection, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
